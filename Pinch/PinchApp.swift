43b2a9ecb77759032713019238fb0e52108610d6
shagun-nickelfox <shagun.bansal@nickelfox.com>
2024-01-16 22:55:12 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Nickelfox on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
